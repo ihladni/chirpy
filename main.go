373c7aec6d3b70463b85974456318f29ab0fcfd0
ihladni <ivan.hladni@gmail.com>
2025-02-10 21:35:42 +0100

    CH01-L04

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux() // *ServeMux
+
+	var srv *http.Server = &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		println(err)
+	}
+}
