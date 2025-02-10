50e832eb787607d788e1718e734512a2eb3864f1
ihladni <ivan.hladni@gmail.com>
2025-02-10 21:47:19 +0100

    CH01-L04 additional logging

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
+// build cmd: go build -o out && ./out
+
 func main() {
+	var port string = "8080"
 	mux := http.NewServeMux() // *ServeMux
 
 	var srv *http.Server = &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	err := srv.ListenAndServe()
-	if err != nil {
-		println(err)
-	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+	/*
+		err := srv.ListenAndServe()
+		if err != nil {
+			println(err)
+		}
+	*/
 }
