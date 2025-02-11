85be5e0db4695510e94b850669765a6d85bc1410
ihladni <ivan.hladni@gmail.com>
2025-02-11 21:04:10 +0100

    img added

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 )
 
 // build cmd: go build -o out && ./out
+// curl -o chirpy-logo.png https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/2CofkLc.png
+
+type healthHandler struct{}
+
+func (handler healthHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("OK"))
+}
 
 func main() {
 	var port string = "8080"
@@ -16,12 +25,12 @@ func main() {
 		Handler: mux,
 	}
 
+	var file_server http.Handler = http.FileServer(http.Dir("."))
+
+	mux.Handle("/healthz", healthHandler{})
+	// mux.Handle("/", file_server)
+	mux.Handle("/app/", http.StripPrefix("/app", file_server))
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
-	/*
-		err := srv.ListenAndServe()
-		if err != nil {
-			println(err)
-		}
-	*/
 }
